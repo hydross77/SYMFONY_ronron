4994bc68aa3ed07dc4ebc4b8140fd323e4c0771a
Boni77 <tiffany.dellmann@gmail.com>
2023-03-30 13:09:03 +0200

    CSS navbar + footer OK

--- src/Controller/DonationController.php
+++ src/Controller/DonationController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DonationController extends AbstractController
 {
 
-    #[Route('/donation', name: 'app_donation')]
+    #[Route('/donation', name: 'app_donation_stripe')]
     public function index(Request $request): RedirectResponse
     {
         $url = 'https://buy.stripe.com/test_14kg2abmEdAZem4dQQ';
