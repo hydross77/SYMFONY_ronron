7ebaffe3c3833d27810cdde14c51d5b96732407b
johan <johan.kasri@icloud.com>
2023-04-07 14:18:20 +0200

    api google

--- public/js/script.js
+++ public/js/script.js
@@ -54,3 +54,12 @@ function toggleFavorite(catId) {
         });
     }
 }
+
+/** Autocompletion **/
+
+const input = document.getElementById('city');
+var autocomplete = new google.maps.places.Autocomplete(input);
+google.maps.event.addListener(autocomplete, 'place_changed', function(){
+    const place = autocomplete.getPlace();
+    input.value = place.name
+});
